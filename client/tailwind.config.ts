494961be39de458e0143ae353c7d8c3b70821e78
cbvdev <cbvware@gmail.com>
2024-12-01 20:26:51 +0200

    auth

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -10,14 +10,6 @@ export default {
       md: '1024px',
       lg: '1280px',
     },
-    spacing: {
-      '1': '8px',
-      '2': '12px',
-      '3': '16px',
-      '4': '24px',
-      '5': '32px',
-      '6': '48px',
-    },
   },
   darkMode: 'class',
   plugins: [
