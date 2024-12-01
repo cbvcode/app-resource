572ae373a81a87affbd76beb648538a26811231c
cbvdev <cbvware@gmail.com>
2024-12-01 21:24:24 +0200

    auth

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,6 +3,7 @@ import { nextui } from '@nextui-org/react'
 import type { Config } from 'tailwindcss'
 
 export default {
+  mode: 'jit',
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
