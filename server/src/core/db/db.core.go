94132aa8af952fd7c92922826c0ea143c978dd52
cbvdev <cbvware@gmail.com>
2024-12-03 01:46:55 +0200

    auth

--- server/src/core/db/db.core.go
+++ server/src/core/db/db.core.go
@@ -13,9 +13,10 @@ func InitDB() {
 	dsn := config.DatabaseUrl
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger:      logger.Default.LogMode(logger.Info),
-		PrepareStmt: true,
-		QueryFields: true,
+		Logger:                 logger.Default.LogMode(logger.Info),
+		PrepareStmt:            true,
+		QueryFields:            true,
+		SkipDefaultTransaction: true,
 	})
 	if err != nil {
 		panic("Failed to connect to database")
